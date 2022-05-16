436837c6752edeba541176f5b39722ab3051599e
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-16 13:15:45 +0200

    Move fixtures to conftest

---
+++ tests/slack_app/conftest.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import clear_mappers, sessionmaker
+
+from slack_app.orm import metadata, start_mappers
+
+
+@pytest.fixture  # type: ignore[misc]
+def in_memory_db() -> Any:
+    engine = create_engine("sqlite:///:memory:")
+    metadata.create_all(engine)
+    return engine
+
+
+@pytest.fixture  # type: ignore[misc]
+def session(in_memory_db: Any) -> Any:
+    start_mappers()
+    yield sessionmaker(bind=in_memory_db)()
+    clear_mappers()
