00d49fc09e34b752d3ba5a0cc702a7724ca24014
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-16 13:26:16 +0200

    Add sqlalchemy repository

---
+++ src/slack_app/repository.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from sqlalchemy.orm import Session
+
+from slack_app.model import Channel, Keyword, User
+
+
+class AbstractRepository(ABC):
+    @abstractmethod
+    def add(self, keyword: Keyword) -> None:
+        pass
+
+    @abstractmethod
+    def get(self, channel: Channel, user: User) -> List[Keyword]:
+        pass
+
+
+class SQLAlchemyRepository(AbstractRepository):
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    def add(self, keyword: Keyword) -> None:
+        self.session.add(keyword)
+
+    def get(self, channel: Channel, user: User) -> List[Keyword]:
+        return self.session.query(Keyword).filter_by(channel=channel, user=user).all()
