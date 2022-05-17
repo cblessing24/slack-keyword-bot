a1236beabc5a8e338d4731954f6b02cb5c771e94
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-17 16:53:18 +0200

    Add service that gets subscribers

--- src/slack_app/service_layer/services.py
+++ src/slack_app/service_layer/services.py
@@ -1,9 +1,16 @@
+from typing import Set
+
 from sqlalchemy.orm.session import Session
 
 from ..adapters.repository import AbstractRepository
-from ..domain.model import Keyword
+from ..domain.model import Keyword, Message, User
 
 
 def add_keyword(repo: AbstractRepository, session: Session, keyword: Keyword) -> None:
     repo.add(keyword)
     session.commit()
+
+
+def get_subscribers(repo: AbstractRepository, message: Message) -> Set[User]:
+    keywords = repo.get(message.channel)
+    return {k.user for k in keywords if k in message}
