4d5f92e94027ce5db61c2464576637948f94ef47
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-17 14:22:40 +0200

    Add fixture for creating repos

--- tests/slack_app/service_layer/test_services.py
+++ tests/slack_app/service_layer/test_services.py
@@ -1,4 +1,6 @@
-from typing import Iterable, List, Optional
+from typing import Iterable, List, Optional, Protocol, Set
+
+import pytest
 
 from slack_app.adapters.repository import AbstractRepository
 from slack_app.domain.model import Channel, Keyword, User, Word
@@ -6,14 +8,27 @@ from slack_app.service_layer.services import add_keyword
 
 
 class FakeRepository(AbstractRepository):
-    def __init__(self, keywords: Optional[Iterable[Keyword]] = None) -> None:
-        self.keywords = set(keywords) if keywords else set()
+    def __init__(self, keywords: Set[Keyword]) -> None:
+        self.keywords = keywords
 
     def add(self, keyword: Keyword) -> None:
         self.keywords.add(keyword)
 
     def get(self, channel: Channel, user: User) -> List[Keyword]:
         return [k for k in self.keywords if k.channel == channel and k.user == user]
+
+
+class RepositoryCreator(Protocol):
+    def __call__(self, keywords: Optional[Set[Keyword]] = None) -> FakeRepository:
+        ...
+
+
+@pytest.fixture
+def create_repo() -> RepositoryCreator:
+    def create(keywords: Optional[Iterable[Keyword]] = None) -> FakeRepository:
+        return FakeRepository(set(keywords) if keywords else set())
+
+    return create
 
 
 class FakeSession:
@@ -23,17 +38,16 @@ class FakeSession:
         self.committed = True
 
 
-def test_keyword_gets_added_to_repository() -> None:
-    repo = FakeRepository()
+def test_keyword_gets_added_to_repository(create_repo: RepositoryCreator) -> None:
+    repo = create_repo()
     session = FakeSession()
     keyword = Keyword(channel=Channel("general"), user=User("bob"), word=Word("hello"))
     add_keyword(repo, session, keyword)
     assert repo.get(keyword.channel, keyword.user) == [keyword]
 
 
-def test_added_keyword_gets_committed() -> None:
-    repo = FakeRepository()
+def test_added_keyword_gets_committed(create_repo: RepositoryCreator) -> None:
     session = FakeSession()
     keyword = Keyword(channel=Channel("general"), user=User("bob"), word=Word("hello"))
-    add_keyword(repo, session, keyword)
+    add_keyword(create_repo(), session, keyword)
     assert session.committed
