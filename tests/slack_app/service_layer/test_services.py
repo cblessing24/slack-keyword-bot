53732438ec6daeb76e87606e411fd5212add4914
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-17 16:30:30 +0200

    Move fake repo to conftest

--- tests/slack_app/service_layer/test_services.py
+++ tests/slack_app/service_layer/test_services.py
@@ -1,34 +1,7 @@
-from typing import Iterable, List, Optional, Protocol, Set
-
-import pytest
-
-from slack_app.adapters.repository import AbstractRepository
 from slack_app.domain.model import Channel, Keyword, User, Word
 from slack_app.service_layer.services import add_keyword
 
-
-class FakeRepository(AbstractRepository):
-    def __init__(self, keywords: Set[Keyword]) -> None:
-        self.keywords = keywords
-
-    def add(self, keyword: Keyword) -> None:
-        self.keywords.add(keyword)
-
-    def get(self, channel: Channel, user: User) -> List[Keyword]:
-        return [k for k in self.keywords if k.channel == channel and k.user == user]
-
-
-class RepositoryCreator(Protocol):
-    def __call__(self, keywords: Optional[Set[Keyword]] = None) -> FakeRepository:
-        ...
-
-
-@pytest.fixture
-def create_repo() -> RepositoryCreator:
-    def create(keywords: Optional[Iterable[Keyword]] = None) -> FakeRepository:
-        return FakeRepository(set(keywords) if keywords else set())
-
-    return create
+from .conftest import RepositoryCreator
 
 
 class FakeSession:
