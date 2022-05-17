731b2b9747d7d8c5f60e02a61635ce1d0423cf82
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-17 13:49:58 +0200

    Add keyword creating service

--- src/slack_app/services.py
+++ src/slack_app/services.py
@@ -1,2 +1,9 @@
-def add_keyword(keyword) -> None:
-    pass
+from sqlalchemy.orm.session import Session
+
+from .model import Keyword
+from .repository import AbstractRepository
+
+
+def add_keyword(repo: AbstractRepository, session: Session, keyword: Keyword) -> None:
+    repo.add(keyword)
+    session.commit()
