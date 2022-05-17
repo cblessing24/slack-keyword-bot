573850bca5df568f3538cb7b9b3973b69c48f9c7
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-17 13:49:46 +0200

    Add tests for keyword creating service

---
+++ tests/slack_app/test_services.py
@@ -0,0 +1,39 @@
+from typing import List, Set
+
+from slack_app.model import Channel, Keyword, User, Word
+from slack_app.repository import AbstractRepository
+from slack_app.services import add_keyword
+
+
+class FakeRepository(AbstractRepository):
+    def __init__(self) -> None:
+        self.keywords: Set[Keyword] = set()
+
+    def add(self, keyword: Keyword) -> None:
+        self.keywords.add(keyword)
+
+    def get(self, channel: Channel, user: User) -> List[Keyword]:
+        return [k for k in self.keywords if k.channel == channel and k.user == user]
+
+
+class FakeSession:
+    committed = False
+
+    def commit(self) -> None:
+        self.committed = True
+
+
+def test_keyword_gets_added_to_repository() -> None:
+    repo = FakeRepository()
+    session = FakeSession()
+    keyword = Keyword(channel=Channel("general"), user=User("bob"), word=Word("hello"))
+    add_keyword(repo, session, keyword)
+    assert repo.get(keyword.channel, keyword.user) == [keyword]
+
+
+def test_added_keyword_gets_committed() -> None:
+    repo = FakeRepository()
+    session = FakeSession()
+    keyword = Keyword(channel=Channel("general"), user=User("bob"), word=Word("hello"))
+    add_keyword(repo, session, keyword)
+    assert session.committed
