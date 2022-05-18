4405de821a22fd0d6e829dd4cb533282d36a22ec
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-18 16:51:29 +0200

    Move test for service getting subscribers

--- tests/slack_app/service_layer/test_services.py
+++ tests/slack_app/service_layer/test_services.py
@@ -1,4 +1,4 @@
-from slack_app.service_layer.services import add_keyword, list_keywords
+from slack_app.service_layer.services import add_keyword, get_subscribers, list_keywords
 
 from .conftest import FakeRepositoryCreator, FakeSession
 
@@ -13,3 +13,17 @@ def test_keyword_gets_added(create_repo: FakeRepositoryCreator, session: FakeSes
 def test_added_keyword_gets_committed(create_repo: FakeRepositoryCreator, session: FakeSession) -> None:
     add_keyword(create_repo(), session, channel="general", user="bob", word="hello")
     assert session.committed
+
+
+def test_subscribers_are_returned(create_repo: FakeRepositoryCreator, session: FakeSession) -> None:
+    repo = create_repo()
+    keywords = [
+        ("general", "bob", "Hello"),
+        ("general", "bob", "World"),
+        ("general", "alice", "World"),
+        ("random", "alice", "World"),
+    ]
+    for keyword in keywords:
+        add_keyword(repo, session, *keyword)
+    subscribers = get_subscribers(repo, channel="general", user="john", text="Goddbye World")
+    assert subscribers == {"bob", "alice"}
