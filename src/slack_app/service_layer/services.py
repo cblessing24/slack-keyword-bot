a7ddc385e5ed58b66c654572ae121659bc7eaf72
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-19 13:48:30 +0200

    Use domain service in service getting subscribers

--- src/slack_app/service_layer/services.py
+++ src/slack_app/service_layer/services.py
@@ -14,7 +14,7 @@ def add_keyword(repo: AbstractRepository, session: Session, channel: str, user: 
 def get_subscribers(repo: AbstractRepository, channel: str, author: str, text: str) -> Set[str]:
     keywords = repo.get(model.Channel(channel))
     message = model.Message(model.Channel(channel), model.User(author), model.Text(text))
-    return {k.subscriber for k in keywords if k in message}
+    return set(model.get_subscribers(message, keywords))
 
 
 def list_keywords(repo: AbstractRepository, channel: str, subscriber: str) -> Set[str]:
