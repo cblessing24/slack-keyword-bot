7a5e6e89cdcea28881c80d23300801a4a3758abc
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-19 13:43:12 +0200

    Add model import

--- src/slack_app/service_layer/services.py
+++ src/slack_app/service_layer/services.py
@@ -3,20 +3,20 @@ from typing import Set
 from sqlalchemy.orm.session import Session
 
 from ..adapters.repository import AbstractRepository
-from ..domain.model import Channel, Keyword, Message, Text, User, Word
+from ..domain import model
 
 
 def add_keyword(repo: AbstractRepository, session: Session, channel: str, user: str, word: str) -> None:
-    repo.add(Keyword(Channel(channel), User(user), Word(word)))
+    repo.add(model.Keyword(model.Channel(channel), model.User(user), model.Word(word)))
     session.commit()
 
 
 def get_subscribers(repo: AbstractRepository, channel: str, author: str, text: str) -> Set[str]:
-    keywords = repo.get(Channel(channel))
-    message = Message(Channel(channel), User(author), Text(text))
+    keywords = repo.get(model.Channel(channel))
+    message = model.Message(model.Channel(channel), model.User(author), model.Text(text))
     return {k.subscriber for k in keywords if k in message}
 
 
 def list_keywords(repo: AbstractRepository, channel: str, subscriber: str) -> Set[str]:
-    keywords = repo.get(Channel(channel))
-    return {k.word for k in keywords if k.subscriber == User(subscriber)}
+    keywords = repo.get(model.Channel(channel))
+    return {k.word for k in keywords if k.subscriber == model.User(subscriber)}
