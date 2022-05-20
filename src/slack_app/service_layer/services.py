c5d979b2a6a2c5c3f6fd4bc0e052087d7266a984
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-20 15:57:39 +0200

    Use uow in services

--- src/slack_app/service_layer/services.py
+++ src/slack_app/service_layer/services.py
@@ -1,22 +1,21 @@
 from typing import Set
 
-from sqlalchemy.orm.session import Session
-
-from ..adapters.repository import AbstractRepository
 from ..domain import model
+from .unit_of_work import AbstractUnitOfWork, R
 
 
-def add_keyword(repo: AbstractRepository, session: Session, channel: str, user: str, word: str) -> None:
-    repo.add(model.Keyword(model.Channel(channel), model.User(user), model.Word(word)))
-    session.commit()
+def add_keyword(uow: AbstractUnitOfWork[R], channel: str, user: str, word: str) -> None:
+    with uow:
+        uow.keywords.add(model.Keyword(model.Channel(channel), model.User(user), model.Word(word)))
+        uow.commit()
 
 
-def get_subscribers(repo: AbstractRepository, channel: str, author: str, text: str) -> Set[str]:
-    keywords = repo.get(model.Channel(channel))
+def get_subscribers(uow: AbstractUnitOfWork[R], channel: str, author: str, text: str) -> Set[str]:
+    keywords = uow.keywords.get(model.Channel(channel))
     message = model.Message(model.Channel(channel), model.User(author), model.Text(text))
     return set(model.get_subscribers(message, keywords))
 
 
-def list_keywords(repo: AbstractRepository, channel: str, subscriber: str) -> Set[str]:
-    keywords = repo.get(model.Channel(channel))
+def list_keywords(uow: AbstractUnitOfWork[R], channel: str, subscriber: str) -> Set[str]:
+    keywords = uow.keywords.get(model.Channel(channel))
     return {k.word for k in keywords if k.subscriber == model.User(subscriber)}
