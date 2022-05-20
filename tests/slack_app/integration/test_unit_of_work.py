c83e226e511a47f255396ba8a475730019ca5ab1
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-20 10:33:02 +0200

    Add integration tests for sqlalchemy uow

---
+++ tests/slack_app/integration/test_unit_of_work.py
@@ -0,0 +1,42 @@
+from typing import Any
+
+import pytest
+
+from slack_app.service_layer.unit_of_work import SQLAlchemyUnitOfWork
+
+from ..conftest import KeywordCreator
+
+
+def test_can_insert_keyword(session_factory: Any, create_keyword: KeywordCreator) -> None:
+    uow = SQLAlchemyUnitOfWork(session_factory)
+    with uow:
+        uow.keywords.add(create_keyword(channel="general", subscriber="bob", word="hello"))
+        uow.commit()
+
+    session = session_factory()
+    assert list(session.execute("SELECT * FROM 'keyword'")) == [(1, "general", "bob", "hello")]
+
+
+def test_rolls_back_changes_by_default(session_factory: Any, create_keyword: KeywordCreator) -> None:
+    uow = SQLAlchemyUnitOfWork(session_factory)
+    with uow:
+        keyword = create_keyword()
+        uow.keywords.add(keyword)
+
+    session = session_factory()
+    assert list(session.execute("SELECT * FROM 'keyword'")) == []
+
+
+def test_rolls_back_on_error(session_factory: Any, create_keyword: KeywordCreator) -> None:
+    class MyException(Exception):
+        pass
+
+    uow = SQLAlchemyUnitOfWork(session_factory)
+    with pytest.raises(MyException):
+        with uow:
+            keyword = create_keyword()
+            uow.keywords.add(keyword)
+            raise MyException()
+
+    session = session_factory()
+    assert list(session.execute("SELECT * FROM 'keyword'")) == []
