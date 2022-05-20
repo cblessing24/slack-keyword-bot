f85ad26621706a431cf409de201f4edcc75f9090
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-20 10:34:44 +0200

    Add sqlalchemy uow

---
+++ src/slack_app/service_layer/unit_of_work.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from types import TracebackType
+from typing import Any, Generic, Optional, Type, TypeVar
+
+from ..adapters.repository import AbstractRepository, SQLAlchemyRepository
+
+R = TypeVar("R", bound=AbstractRepository)
+
+
+class AbstractUnitOfWork(ABC, Generic[R]):
+    keywords: R
+
+    def __enter__(self) -> AbstractUnitOfWork[R]:
+        return self
+
+    def __exit__(
+        self, exc_type: Optional[Type[BaseException]], exc: Optional[BaseException], traceback: Optional[TracebackType]
+    ) -> None:
+        self.rollback()
+
+    @abstractmethod
+    def commit(self) -> None:
+        pass
+
+    @abstractmethod
+    def rollback(self) -> None:
+        pass
+
+
+class SQLAlchemyUnitOfWork(AbstractUnitOfWork[SQLAlchemyRepository]):
+    def __init__(self, session_factory: Any) -> None:
+        self.session_factory = session_factory
+        self._session: Optional[Any] = None
+
+    @property
+    def session(self) -> Any:
+        if self._session is None:
+            raise RuntimeError("Session is not initialized")
+        return self._session
+
+    def __enter__(self) -> AbstractUnitOfWork[SQLAlchemyRepository]:
+        session = self.session_factory()
+        self.keywords = SQLAlchemyRepository(session)
+        self._session = session
+        return super().__enter__()
+
+    def __exit__(
+        self, exc_type: Optional[Type[BaseException]], exc: Optional[BaseException], traceback: Optional[TracebackType]
+    ) -> None:
+        super().__exit__(exc_type, exc, traceback)
+        self.session.close()
+
+    def commit(self) -> None:
+        self.session.commit()
+
+    def rollback(self) -> None:
+        self.session.rollback()
