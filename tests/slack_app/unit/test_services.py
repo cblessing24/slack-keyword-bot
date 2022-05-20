d493cc9057a98e14126b65e7ca7dff16758f7cca
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-20 15:56:47 +0200

    Change service tests to use uow

--- tests/slack_app/unit/test_services.py
+++ tests/slack_app/unit/test_services.py
@@ -1,27 +1,42 @@
 from slack_app.service_layer.services import add_keyword, get_subscribers, list_keywords
+from slack_app.service_layer.unit_of_work import AbstractUnitOfWork
 
-from .conftest import FakeRepositoryCreator, FakeSession
+from .conftest import FakeRepository
 
 
-def test_keyword_gets_added(create_repo: FakeRepositoryCreator, session: FakeSession) -> None:
-    repo = create_repo()
-    add_keyword(repo, session, channel="general", user="bob", word="hello")
-    keywords = list_keywords(repo, channel="general", subscriber="bob")
+class FakeUnitOfWork(AbstractUnitOfWork[FakeRepository]):
+    def __init__(self) -> None:
+        self.committed = False
+        self.keywords = FakeRepository()
+
+    def commit(self) -> None:
+        self.committed = True
+
+    def rollback(self) -> None:
+        pass
+
+
+def test_keyword_gets_added() -> None:
+    uow = FakeUnitOfWork()
+    add_keyword(uow, channel="general", user="bob", word="hello")
+    keywords = list_keywords(uow, channel="general", subscriber="bob")
     assert keywords == {"hello"}
 
 
-def test_added_keyword_gets_committed(create_repo: FakeRepositoryCreator, session: FakeSession) -> None:
-    add_keyword(create_repo(), session, channel="general", user="bob", word="hello")
-    assert session.committed
+def test_added_keyword_gets_committed() -> None:
+    uow = FakeUnitOfWork()
+    add_keyword(uow, channel="general", user="bob", word="hello")
+    assert issubclass(FakeUnitOfWork, AbstractUnitOfWork)
+    assert uow.committed
 
 
-def test_subscribers_are_returned(create_repo: FakeRepositoryCreator, session: FakeSession) -> None:
-    repo = create_repo()
+def test_subscribers_are_returned() -> None:
+    uow = FakeUnitOfWork()
     in_keyword = ("general", "bob", "World")
     out_keyword = ("general", "alice", "World")
     author_keyword = ("general", "john", "Goodbye")
     keywords = [in_keyword, out_keyword, author_keyword]
     for keyword in keywords:
-        add_keyword(repo, session, *keyword)
-    subscribers = get_subscribers(repo, channel="general", author="john", text="Goodbye World")
+        add_keyword(uow, *keyword)
+    subscribers = get_subscribers(uow, channel="general", author="john", text="Goodbye World")
     assert subscribers == {"bob", "alice"}
