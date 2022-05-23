6c7817fc53db559e41ad54c462e8060cb06d88e8
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-23 16:27:53 +0200

    Add updated slack app entrypoint

--- src/slack_app/entrypoints/slack_app.py
+++ src/slack_app/entrypoints/slack_app.py
@@ -4,18 +4,18 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 from dotenv import load_dotenv
+from flask import Flask, request
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
 
+from ..adapters.orm import start_mappers
+from ..service_layer.services import add_keyword, get_subscribers
+from ..service_layer.unit_of_work import SQLAlchemyUnitOfWork
+
 load_dotenv()
+start_mappers()
 
 app = App()
-
-notifications = {}
-
-
-def create_notification(channel: str, user: str, keyword: str):
-    pass
 
 
 @app.middleware
@@ -26,21 +26,20 @@ def log_request(logger, body, next):
 
 @app.event("message")
 def event_message(event, client):
-    for keyword, user_id in notifications.items():
-        if keyword in event["text"]:
-            quoted = "> " + "\n> ".join(event["text"].split("\n"))
-            text = f"<@{event['user']}> mentioned '{keyword}' in <#{event['channel']}>:\n{quoted}"
-            client.chat_postMessage(channel=user_id, text=text)
+    for subscriber in get_subscribers(
+        SQLAlchemyUnitOfWork(), channel=event["channel"], author=event["user"], text=event["text"]
+    ):
+        quoted = "> " + "\n> ".join(event["text"].split("\n"))
+        text = f"<@{event['user']}> mentioned a keyword in <#{event['channel']}>:\n{quoted}"
+        client.chat_postMessage(channel=subscriber, text=text)
 
 
 @app.command("/notify")
 def command_notify(ack, command, respond):
     ack()
-    create_notification(channel=command["channel_id"], user=command["user_id"], keyword=command["text"])
+    add_keyword(SQLAlchemyUnitOfWork(), channel=command["channel_id"], user=command["user_id"], word=command["text"])
     respond(f"You will be notified if '{command['text']}' is mentioned in <#{command['channel_id']}>!")
 
-
-from flask import Flask, request
 
 flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
