5795935b90f4e88fa1b38122e4278ceb31b66185
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-05-27 15:08:53 +0200

    Add channel table to orm

--- src/slack_app/adapters/orm.py
+++ src/slack_app/adapters/orm.py
@@ -1,16 +1,18 @@
-from sqlalchemy import Boolean, Column, Integer, String, Table
-from sqlalchemy.orm import registry
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import registry, relationship
 
-from ..domain.model import Keyword
+from ..domain.model import Channel, Keyword
 
 mapper_registry = registry()
 metadata = mapper_registry.metadata
+
+channel = Table("channel", metadata, Column("channel_name", String(255), primary_key=True))
 
 keyword = Table(
     "keyword",
     metadata,
     Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("channel_name", String(255)),
+    Column("channel_name", ForeignKey("channel.channel_name")),
     Column("subscriber", String(255)),
     Column("word", String(255)),
     Column("active", Boolean),
@@ -19,3 +21,4 @@ keyword = Table(
 
 def start_mappers() -> None:
     mapper_registry.map_imperatively(Keyword, keyword)
+    mapper_registry.map_imperatively(Channel, channel, properties={"keywords": relationship(Keyword)})
