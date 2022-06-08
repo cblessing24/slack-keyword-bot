6d7bf5f8a6f22cc8d15e4b1cd87025ded1fa707f
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-06-08 14:21:03 +0200

    Convert message bus to class

--- src/slack_app/service_layer/messagebus.py
+++ src/slack_app/service_layer/messagebus.py
@@ -1,51 +1,59 @@
 import logging
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 from ..domain import commands, events
-from . import handlers, unit_of_work
+
+if TYPE_CHECKING:
+    from .handlers import CommandHandlerMap, EventHandlerMap
+    from .unit_of_work import AbstractUnitOfWork, R
 
 logger = logging.getLogger(__name__)
 
 Message = events.Event | commands.Command
 
 
-def handle(message: Message, uow: unit_of_work.AbstractUnitOfWork[unit_of_work.R]) -> list[Any]:
-    results: list[Any] = []
-    queue = [message]
-    while queue:
-        message = queue.pop(0)
-        if isinstance(message, events.Event):
-            handle_event(message, queue, uow)
-        elif isinstance(message, commands.Command):
-            cmd_result = handle_command(message, queue, uow)
-            results.append(cmd_result)
-        else:
-            raise TypeError(f"Unknown message type: {type(message)}")
-    return results
+class MessageBus:
+    def __init__(
+        self,
+        uow: AbstractUnitOfWork[R],
+        event_handlers: EventHandlerMap,
+        command_handlers: CommandHandlerMap,
+    ) -> None:
+        self.uow = uow
+        self.event_handlers = event_handlers
+        self.command_handlers = command_handlers
 
+    def handle(self, message: Message) -> list[Any]:
+        results: list[Any] = []
+        queue = [message]
+        while queue:
+            message = queue.pop(0)
+            if isinstance(message, events.Event):
+                self.handle_event(message, queue)
+            elif isinstance(message, commands.Command):
+                cmd_result = self.handle_command(message, queue)
+                results.append(cmd_result)
+            else:
+                raise TypeError(f"Unknown message type: {type(message)}")
+        return results
 
-def handle_event(
-    event: events.Event, queue: list[Message], uow: unit_of_work.AbstractUnitOfWork[unit_of_work.R]
-) -> None:
-    for handler in handlers.EVENT_HANDLERS[type(event)]:
-        logger.debug(f"Handling event {event!r} with handler {handler!r}")
+    def handle_event(self, event: events.Event, queue: list[Message]) -> None:
+        for handler in self.event_handlers[type(event)]:
+            logger.debug(f"Handling event {event!r} with handler {handler!r}")
+            try:
+                handler(event, uow=self.uow)
+                queue.extend(self.uow.collect_new_events())
+            except Exception:
+                logger.exception(f"Exception handling event {event!r}")
+                continue
+
+    def handle_command(self, command: commands.Command, queue: list[Message]) -> Any:
+        logger.debug(f"Handling command {command!r}")
         try:
-            handler(event, uow=uow)
-            queue.extend(uow.collect_new_events())
+            handler = self.command_handlers[type(command)]
+            result = handler(command, uow=self.uow)
+            queue.extend(self.uow.collect_new_events())
+            return result
         except Exception:
-            logger.exception(f"Exception handling event {event!r}")
-            continue
-
-
-def handle_command(
-    command: commands.Command, queue: list[Message], uow: unit_of_work.AbstractUnitOfWork[unit_of_work.R]
-) -> Any:
-    logger.debug(f"Handling command {command!r}")
-    try:
-        handler = handlers.COMMAND_HANDLERS[type(command)]
-        result = handler(command, uow=uow)
-        queue.extend(uow.collect_new_events())
-        return result
-    except Exception:
-        logger.exception(f"Exception handling command {command!r}")
-        raise
+            logger.exception(f"Exception handling command {command!r}")
+            raise
