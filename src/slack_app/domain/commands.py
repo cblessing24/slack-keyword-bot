2a6c2e356f46da9f3480df791f10494845680434
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-06-08 12:26:55 +0200

    Add commands

---
+++ src/slack_app/domain/commands.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Command:
+    """Base class for all commands."""
+
+
+@dataclass(frozen=True)
+class Subscribe(Command):
+    channel_name: str
+    subscriber: str
+    keyword: str
+
+
+@dataclass(frozen=True)
+class Unsubscribe(Command):
+    channel_name: str
+    subscriber: str
+    keyword: str
+
+
+@dataclass(frozen=True)
+class ListSubscribers(Command):
+    channel_name: str
+    author: str
+    text: str
+
+
+@dataclass(frozen=True)
+class ListSubscriptions(Command):
+    channel_name: str
+    subscriber: str
