7be9a3de9d1889687a5f1168c601d09e235d4318
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-06-09 15:46:43 +0200

    Do not start mappers every time

--- src/slack_app/entrypoints/slack_app/__init__.py
+++ src/slack_app/entrypoints/slack_app/__init__.py
@@ -4,12 +4,10 @@ import os
 from dotenv import load_dotenv
 from slack_bolt import App
 
-from ...adapters.orm import start_mappers
 from .components import components
 
 logging.basicConfig(level=getattr(logging, os.environ.get("LOGLEVEL", "WARNING")))
 load_dotenv()
-start_mappers()
 
 
 def configure_app(app: App) -> None:
