90fb41b5106ddceb08fd03fd6efdb8299c7190c1
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-06-16 17:34:21 +0200

    Inject dependencies

--- src/slack_app/bootstrap.py
+++ src/slack_app/bootstrap.py
@@ -4,7 +4,14 @@ from typing import Any, Mapping, Optional, cast
 
 from .adapters import orm
 from .adapters.notifications import AbstractNotifications
-from .service_layer.handlers import COMMAND_HANDLERS, EVENT_HANDLERS, M, MessageHandler
+from .service_layer.handlers import (
+    COMMAND_HANDLERS,
+    EVENT_HANDLERS,
+    CommandHandlerMap,
+    EventHandlerMap,
+    M,
+    MessageHandler,
+)
 from .service_layer.messagebus import MessageBus, U
 
 
@@ -13,7 +20,26 @@ def bootstrap(
 ) -> MessageBus[U]:
     if start_mappers:
         orm.start_mappers()
-    return cast(MessageBus[U], MessageBus(uow, event_handlers=EVENT_HANDLERS, command_handlers=COMMAND_HANDLERS))
+
+    dependencies = {"uow": uow}
+    injected_command_handlers = cast(
+        CommandHandlerMap,
+        {
+            command_type: inject_dependencies(handler, dependencies)
+            for command_type, handler in COMMAND_HANDLERS.items()
+        },
+    )
+    injected_event_handlers = cast(
+        EventHandlerMap,
+        {
+            event_type: [inject_dependencies(handler, dependencies) for handler in event_handlers]
+            for event_type, event_handlers in EVENT_HANDLERS.items()
+        },
+    )
+    return cast(
+        MessageBus[U],
+        MessageBus(uow, event_handlers=injected_event_handlers, command_handlers=injected_command_handlers),
+    )
 
 
 def inject_dependencies(handler: MessageHandler[M], dependencies: Mapping[str, Any]) -> MessageHandler[M]:
