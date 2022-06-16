9ae3ca0abb1cf0281331ef9986ecbda9e2cd2184
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-06-16 17:27:04 +0200

    Add dependency injecting function

--- src/slack_app/bootstrap.py
+++ src/slack_app/bootstrap.py
@@ -1,8 +1,10 @@
-from typing import Optional, cast
+import functools
+import inspect
+from typing import Any, Mapping, Optional, cast
 
 from .adapters import orm
 from .adapters.notifications import AbstractNotifications
-from .service_layer.handlers import COMMAND_HANDLERS, EVENT_HANDLERS
+from .service_layer.handlers import COMMAND_HANDLERS, EVENT_HANDLERS, M, MessageHandler
 from .service_layer.messagebus import MessageBus, U
 
 
@@ -12,3 +14,9 @@ def bootstrap(
     if start_mappers:
         orm.start_mappers()
     return cast(MessageBus[U], MessageBus(uow, event_handlers=EVENT_HANDLERS, command_handlers=COMMAND_HANDLERS))
+
+
+def inject_dependencies(handler: MessageHandler[M], dependencies: Mapping[str, Any]) -> MessageHandler[M]:
+    params = inspect.signature(handler).parameters
+    deps = {name: dependency for name, dependency in dependencies.items() if name in params}
+    return functools.partial(handler, **deps)
