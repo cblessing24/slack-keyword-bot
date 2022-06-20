3377806c4b6d20ffa470787188db6177e69d597e
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-06-20 14:46:03 +0200

    Don't use rm -rf .venv in f-string

--- src/slack_app/service_layer/handlers.py
+++ src/slack_app/service_layer/handlers.py
@@ -62,7 +62,7 @@ Message = Union[commands.Command, events.Event]
 
 def send_subscribed_notification(event: events.Subscribed, notifications: AbstractNotifications) -> None:
     notifications.send(
-        event.subscriber, f"You will be notified if {event.keyword!r} is mentioned in {event.channel_name!r}"
+        event.subscriber, f"You will be notified if '{event.keyword}' is mentioned in {event.channel_name}"
     )
 
 
